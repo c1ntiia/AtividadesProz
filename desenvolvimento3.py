7a7fa43bc1bce1d23a8c2fcb9b7695322df6306b
c1ntiia <scintia280@gmail.com>
2026-06-30 01:11:15 -0300

    desenvolvimento3 atualização

--- desenvolvimento3.py
+++ desenvolvimento3.py
@@ -1,7 +1,19 @@
-print("Decrescente")
+print("Laço WHILE")
 
 andar = 20
 while andar >= 1:
     if andar != 13:
         print(f"{andar}º andar")
     andar -= 1  
+
+print("\nLaço DO-WHILE")
+
+andar = 20
+while True:
+    if andar != 13:
+        print(f"{andar}º andar")
+    
+    andar -= 1
+    
+    if andar < 1:
+        break
